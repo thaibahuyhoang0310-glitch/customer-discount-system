8a7c5558b153e26ae802c2547ba289b593aa0b03
Thai Ba Huy Hoang <thaibahuyhoang0310@gmail.com>
2026-06-11 18:05:45 +0700

    Implement loyalty customer discount feature

--- discount.py
+++ discount.py
@@ -1,15 +1,6 @@
 # discount.py
 
 def calculate_discount(current_total, order_amount):
-    """
-    current_total: Tổng giá trị mua hàng trước đơn hàng hiện tại
-    order_amount: Giá trị đơn hàng hiện tại
-
-    Trả về:
-        0.1 : được giảm giá 10%
-        0   : không được giảm giá
-    """
-
     total_after_order = current_total + order_amount
 
     if total_after_order >= 50000000:
@@ -19,10 +10,6 @@ def calculate_discount(current_total, order_amount):
 
 
 def calculate_payment(current_total, order_amount):
-    """
-    Tính số tiền phải thanh toán sau giảm giá
-    """
-
     discount_rate = calculate_discount(current_total, order_amount)
 
     return order_amount * (1 - discount_rate)
